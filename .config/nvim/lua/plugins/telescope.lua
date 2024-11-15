f668ab1dd64e5c4a4f47722189d9d76dab14e81e
Maxim Khnykin <ebyrdeu.khnykin@gmail.com>
2024-11-15 15:46:53 +0100

    updated

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -6,7 +6,7 @@ return {
 	config = function()
 		require('telescope').setup({
 			defaults = {
-				file_ignore_patterns = { 'node_modules', '.git/', '.idea/', '.vscode/' },
+				file_ignore_patterns = { 'node_modules/', '.git/', '.idea/'},
 			}
 		})
 
