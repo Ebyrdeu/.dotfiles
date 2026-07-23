64a14c9475b4a906382d19d6493e3722773ff2af
Maxim Khnykin <ebyrdeu.khnykin@gmail.com>
2026-07-23 15:52:42 +0200

    updated

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,22 +1,22 @@
 vim.pack.add({
-  { src = git("nvim-lua/plenary.nvim.git") },
-  { src = git("kyazdani42/nvim-web-devicons.git") },
-  { src = git("nvim-telescope/telescope-fzf-native.nvim.git") },
-  { src = git("nvim-telescope/telescope.nvim.git")},
+	{ src = git("nvim-lua/plenary.nvim.git") },
+	{ src = git("kyazdani42/nvim-web-devicons.git") },
+	{ src = git("nvim-telescope/telescope-fzf-native.nvim.git") },
+	{ src = git("nvim-telescope/telescope.nvim.git") },
 })
 
 local telescope = require("telescope")
 telescope.setup({
-  defaults = {
-    file_ignore_patterns = { ".cache", ".git/", "node_modules/" },
-  },
+	defaults = {
+		file_ignore_patterns = { ".cache", ".git/", "node_modules/" },
+	},
 })
 
 pcall(telescope.load_extension, "fzf")
 
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader><leader>", function()
-  builtin.find_files({ hidden = true })
+	builtin.find_files({ hidden = true })
 end, { desc = "Telescope find files" })
 
 vim.keymap.set("n", "<leader>sr", builtin.buffers, { desc = "Telescope buffers" })
